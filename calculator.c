2abbf2cdd045f01d7f95b5455601ff7196daf5bf
uchihaitachi106 <krishnangokula74@gmail.com>
2024-06-04 23:40:13 +0530

    feat:new calculator created

--- calculator.c
+++ calculator.c
@@ -5,9 +5,9 @@ int main(void)
 {
     int n1= get_int("number 1: \n");
     int n2=get_int("number 2: \n");
-    int sum=0;
-    sum= n1+n2;
-    printf("the total sum is:",n1);
+    printf("the sum of the number is: %i\n",n1+n2);
+    float z= (float) n1/(float) n2;
+    printf("the answer is: %f",z);
 }
 
 
