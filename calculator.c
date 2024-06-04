25692ec3709ce702f7238eaa3d2d8e1de52b0e5c
uchihaitachi106 <krishnangokula74@gmail.com>
2024-06-04 19:11:40 +0530

    feat:new file added

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include"cs50.h"
+int main(void)
+{
+    int n1= get_int("number 1: \n");
+    int n2=get_int("number 2: \n");
+    int sum=0;
+    sum= n1+n2;
+    printf("the total sum is:",n1);
+}
+
+
---
+++ hell.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+#include "cs50.h"
+int main(void)
+{
+    printf("hello world\n");
+    int number = get_int("number1:");
+}
